9f5bb7323d3ad527a35e02da25a8987fa58aaa82
Zhai Can <bczhc0@126.com>
2024-12-06 18:06:04 +0800

    to_wolfram: add skipped nodes
    Shitcode!

--- lib.rb
+++ lib.rb
@@ -1,5 +1,5 @@
 class Taxon
-  attr_accessor :rank_display, :display, :sci_name, :skip, :page_url
+  attr_accessor :rank_display, :display, :sci_name, :skip, :page_url, :skipped_sci_names
 
   def initialize(rank_display, display, sci_name, page_url)
     @rank_display = rank_display
@@ -7,6 +7,9 @@ class Taxon
     @sci_name = sci_name
     @skip = false
     @page_url = page_url
+    if @sci_name != nil
+      fail "Invalid #{@sci_name}" unless /^[A-Z][a-zA-Z]+$/.match? @sci_name
+    end
   end
 
   def self.skip
@@ -18,7 +21,14 @@ class Taxon
   # @param other [Taxon]
   def ==(other)
     return false if other == nil
-    @sci_name == other.sci_name
+    cmp_token == other.cmp_token
+  end
+
+  def cmp_token
+    if @skip
+      return skipped_sci_names.join(' ')
+    end
+    @sci_name
   end
 
   def to_s
@@ -27,5 +37,12 @@ class Taxon
 
   def format_chinese_name
     "#{@rank_display}：#{@display}"
+  end
+
+  def format_wolfram_node_name
+    if @skip
+      return '...'
+    end
+    "[#{@rank_display}] #{@display}"
   end
 end
